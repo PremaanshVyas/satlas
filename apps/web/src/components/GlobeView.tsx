127d4305486ed20ea34258e2e700ad65f303858e
PremaanshVyas <premaanshvyas04@gmail.com>
2026-05-15 12:19:45 +1000

    fix(search): remove unused name param in onSelect after double-fire fix

--- apps/web/src/components/GlobeView.tsx
+++ apps/web/src/components/GlobeView.tsx
@@ -115,8 +115,7 @@ export default function GlobeView({
       <div className="absolute top-3 left-1/2 -translate-x-1/2 z-20 pointer-events-auto">
         <SearchBar
           onSearch={searchCatalog}
-          // eslint-disable-next-line @typescript-eslint/no-unused-vars
-          onSelect={(noradId, name) => {
+          onSelect={(noradId) => {
             selectCatalogSatellite(noradId)
           }}
         />
