98a4b59dd5335027e043f79c1cea0d01d97cf434
PremaanshVyas <premaanshvyas04@gmail.com>
2026-05-21 10:33:42 +1000

    test: strengthen CORS block assertion to check None and 400

--- apps/orbital/tests/test_cors.py
+++ apps/orbital/tests/test_cors.py
@@ -35,4 +35,5 @@ def test_cors_blocks_unknown_origin():
             'Access-Control-Request-Method': 'GET',
         },
     )
-    assert resp.headers.get('access-control-allow-origin') != 'https://evil.com'
+    assert resp.headers.get('access-control-allow-origin') is None
+    assert resp.status_code == 400
